3af2a9112884bfefcdfa18d97ea552b088f1a7bc
kemoz7850 <ahamedcool71@gmail.com>
2024-03-07 18:36:30 +0200

    update

--- test.cpp
+++ test.cpp
@@ -13,14 +13,11 @@ int main()
 
     int arr[4];
     int p1,p4;
-    
+    cout << "enter the two opposite points :" << endl;
     for (int i = 0; i <= 3; i++) {
         cin >> arr[i];
+    rect1.points[i]= arr[i]; //p1 x axis
     }
-    rect1.points[0]= arr[0]; //p1 x axis
-    rect1.points[1] = arr[1]; //p1 y axis
-    rect1.points[2] = arr[2]; //p4 x axis
-    rect1.points[3] = arr[3]; //p4 y axis
     cout<<"Enter area:";
     cin>>arr[4];  
     cout << "p2=(" << rect1.points[2] << "," << rect1.points[1] << ")"<<endl;
