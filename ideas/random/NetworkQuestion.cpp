b1dec5fb3a9d9f38c82689ddcb2a8e61495e749a
kemoz7850 <ahamedcool71@gmail.com>
2024-03-09 03:27:46 +0200

    random update

--- ideas/random/NetworkQuestion.cpp
+++ ideas/random/NetworkQuestion.cpp
@@ -21,7 +21,7 @@ int main()
     {
     s=(g==1) ? 'A' : (g==2) ? 'B':'C';
     subnet=(g==1)? rando(8,32):(g==2)?rando(16,32):rando(24,32);
-    cout<<rando(1,255)<<"."<<rando(30,155)<<"."<<rando(20,90)<<"."<<rando(1,255)<<"/"<<subnet<<endl;
+    cout<<rando(1,255)<<"."<<rando(20,200)<<"."<<rando(10,140)<<"."<<rando(1,255)<<"/"<<subnet<<endl;
     cout<<"Class: "<<s<<endl;
     cin>>x;
     }
