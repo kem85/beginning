d5154bc233cf7f579c34399b3c2431dc3dc52d9a
kemoz7850 <ahamedcool71@gmail.com>
2024-03-09 03:09:59 +0200

    Update NetworkQuestion.cpp

--- ideas/random/NetworkQuestion.cpp
+++ ideas/random/NetworkQuestion.cpp
@@ -10,7 +10,6 @@ int rando (int x,int g) //random function
     }
 int main()
 {
-    //test
     char x='y',s;
     int repeat,g,subnet;
     while(true)
@@ -18,12 +17,13 @@ int main()
     sdsd:
     g=rando(1,3);
     if(g==repeat){goto sdsd;}
-    else{
+    else
+    {
     s=(g==1) ? 'A' : (g==2) ? 'B':'C';
     subnet=(g==1)? rando(8,32):(g==2)?rando(16,32):rando(24,32);
     cout<<rando(1,255)<<"."<<rando(30,155)<<"."<<rando(20,90)<<"."<<rando(1,255)<<"/"<<subnet<<endl;
-     cout<<"Class: "<<s<<endl;
-        cin>>x;
+    cout<<"Class: "<<s<<endl;
+    cin>>x;
     }
     repeat=g;
     }
