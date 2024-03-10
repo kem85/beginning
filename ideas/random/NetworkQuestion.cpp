c2f3751daf13104b5ce11507bc5decc6b4cf791c
kemoz7850 <ahamedcool71@gmail.com>
2024-03-10 02:17:39 +0200

    update

--- ideas/random/NetworkQuestion.cpp
+++ ideas/random/NetworkQuestion.cpp
@@ -65,24 +65,3 @@ int main()
     cin>>ip;
     ipcheck(ip);
 }
-//     x=0;
-//     for(int i=0;i<ip.length();i++)
-//     {
-//         cout<<dynamic[x][i]<<endl;
-//         if(i){x++;}
-//     }
-//     while(true)
-//     {
-//         repeat:
-//         g = rando(1, 3);
-//         if (g == repeat) { goto repeat; }
-//         else
-//         {
-//               s = (g == 1) ? 'A' : (g == 2) ? 'B' : 'C';
-//               subnet = (g == 1) ? rando(8, 32) : (g == 2) ? rando(16, 32) : rando(24, 32);
-//               cout << rando(1, 255) << "." << rando(20, 200) << "." << rando(10, 140) << "." << rando(1, 255) << "/"<< subnet << endl;
-//               cout << "Class: " << s << endl;
-//               cin >> x;
-//         }
-//         repeat = g;
-//     }
