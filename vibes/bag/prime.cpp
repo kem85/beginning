13efdd6a56962ee0d6aa05d982fb0f7b0a6100fc
kemoz7850 <ahamedcool71@gmail.com>
2024-04-29 22:47:26 +0300

    4/29/2024
    update of the month

--- vibes/bag/prime.cpp
+++ vibes/bag/prime.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
-int main()
+int main() 
 {
+    //sss
     int num;string prime;
     cout<<"Enter Your Number:";
     cin>>num;
@@ -9,7 +10,7 @@ int main()
     {
         if(num%i==0&&i!=1&&num!=i||num==1)
         {
-            prime="is not prime";break;
+            prime="is not prime";
         }
         else
         {
